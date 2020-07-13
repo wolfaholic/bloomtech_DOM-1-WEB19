ba42f3ef3ee0af46b35cef8178f5f4f8f5caa372
Wolfaholic <melissa.mackie@live.com>
2020-07-13 13:08:39 -0700

    Figured out why page was not displaying fixed it

--- js/index.js
+++ js/index.js
@@ -37,10 +37,6 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
-
 //Images
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
